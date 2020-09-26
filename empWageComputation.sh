d44f28b95aa9a567df685f34956bc78c3ba8b772
Rameez071 <rameezraja405@gmail.com>
2020-09-26 23:54:59 +0530

    Refactor : Employee Hour and Wage defined

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,6 +7,10 @@ randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ];
 then
         echo "Employee is present"
+	empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
 else
         echo "Employee is absent"
+	Salary=0;
 fi
